0384fed379dae721facdf18da2e0b3704ba5769e
flber <42008155+flber@users.noreply.github.com>
2019-07-16 23:16:25 +0200

    Add files via upload
    main.rb

---
+++ main.rb
@@ -0,0 +1,32 @@
+require 'gosu'
+require 'chunky_png'
+
+$width = 640
+$height = 480
+
+class Main < (Eaxample rescue Gosu::Window)
+  def initialize
+    super $width, $height
+    self.caption = "Color Scheme Maker"
+  end
+
+  def update
+  end
+
+  def draw
+  end
+
+  def button_down(id)
+   if id == Gosu::KB_ESCAPE
+     close
+   else
+     super
+   end
+  end
+
+  def needs_cursor?
+    true
+  end
+end
+
+Main.new.show if __FILE__ == $0
